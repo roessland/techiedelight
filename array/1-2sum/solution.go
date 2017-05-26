b9754e4dcf1ad82266d471c6ff75f48c769edcdd
Andreas Røssland <andreas.roessland@gmail.com>
2017-05-26 17:23:44 +0200

    wip: two sum

--- array/1-2sum/solution.go
+++ array/1-2sum/solution.go
@@ -1,9 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"sort"
+)
 
-func TwoSum(arr []int, sum int) (int, int, error) {
-	return 0, 1, nil
+func TwoSum(unsorted []int, sum int) (int, int, error) {
+	arr := make([]int, len(unsorted))
+	copy(arr, unsorted)
+	sort.Ints(arr)
+	for i := 0; i < len(arr); i++ {
+		j := sort.SearchInts(arr, sum-arr[i])
+		if j == len(arr) {
+			continue
+		}
+		if j == i {
+			continue
+		}
+		if arr[i]+arr[j] == sum {
+			return indexOf(unsorted, arr[i]), indexOf(unsorted, arr[j]), nil
+		}
+	}
+	return -1, -1, errors.New("no such pair")
+}
+
+func indexOf(arr []int, n int) int {
+	for i, val := range arr {
+		if val == n {
+			return i
+		}
+	}
+	return -1
 }
 
 func main() {
--- array/1-2sum/solution_test.go
+++ array/1-2sum/solution_test.go
@@ -6,7 +6,44 @@ import "github.com/stretchr/testify/assert"
 
 func TestZeroLength(t *testing.T) {
 	i0, i1, err := TwoSum([]int{}, 10)
-	assert.NotNil(t, err)
 	assert.Equal(t, -1, i0)
 	assert.Equal(t, -1, i1)
+	assert.NotNil(t, err)
+}
+
+func TestEasy(t *testing.T) {
+	arr := []int{1, 2}
+	sum := 3
+	i, j, err := TwoSum(arr, sum)
+	assert.Equal(t, sum, arr[i]+arr[j])
+	assert.NotEqual(t, i, j)
+	assert.Nil(t, err)
+}
+
+func TestSmall(t *testing.T) {
+	arr := []int{8, 7, 2, 5, 3, 1}
+	sum := 10
+	i, j, err := TwoSum(arr, sum)
+	assert.Equal(t, sum, arr[i]+arr[j])
+	assert.NotEqual(t, i, j)
+	assert.Nil(t, err)
+}
+
+func TestNotFound(t *testing.T) {
+	arr := []int{10, 15, 20, 30}
+	sum := 15
+	i, j, err := TwoSum(arr, sum)
+	assert.Equal(t, -1, i)
+	assert.Equal(t, -1, j)
+	assert.NotNil(t, err)
+}
+
+func TestSameNumber(t *testing.T) {
+	arr := []int{10, 10}
+	sum := 20
+	i, j, err := TwoSum(arr, sum)
+	assert.Equal(t, -1, i)
+	assert.Equal(t, -1, j)
+	assert.NotEqual(t, i, j)
+	assert.NotNil(t, err)
 }
